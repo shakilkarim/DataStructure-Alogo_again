4407945e7e9353d38e3020e31819c27c4cdacb69
shakilkarim <rezaul921gcr@gmail.com>
2024-04-23 07:24:54 +0600

    linked list

---
+++ Linked list/create_a_node.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+
+public:
+    int value;
+    Node *next;
+
+    Node(int val)
+    {
+        this->value = val;
+        this->next = NULL;
+    }
+};
+int main()
+{
+
+    return 0;
+}
